b4d0095d9ab13f2c82cb825b8d1125c25a45fe75
Najman <najman.husaini@utah.edu>
2023-03-28 12:23:08 -0600

    Basic layout finished

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "spritecanvas.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    SpriteCanvas *canvas = new SpriteCanvas(this);
-    setCentralWidget(canvas);
 }
 
 MainWindow::~MainWindow()
