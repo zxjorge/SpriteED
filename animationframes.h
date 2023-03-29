44ec16d380aa8be7a9b66b0860d513cd11126f87
Arkein Singh <60797452+singark21@users.noreply.github.com>
2023-03-28 22:59:45 -0600

    Completed animationframes.h and brush.h according to our uml diagram.

--- animationframes.h
+++ animationframes.h
@@ -2,11 +2,20 @@
 #define ANIMATIONFRAMES_H
 
 #include <QObject>
+#include <vector>
+
+using std::vector;
 
 class AnimationFrames
 {
 public:
     AnimationFrames();
+    QImage* addFrame();
+    QImage* getFrame(int index);
+    void deleteFrame(int index);
+    int getFrameCount();
+private:
+    vector<QImage> frames;
 };
 
 #endif // ANIMATIONFRAMES_H
--- brush.h
+++ brush.h
@@ -1,11 +1,27 @@
 #ifndef BRUSH_H
 #define BRUSH_H
-
-
+#include <QColor>
 class Brush
 {
 public:
+
     Brush();
+
+    enum BrushType{
+
+    };
+
+    void setColor(QColor color);
+    void setBrushSize(int size);
+    void setBrushType(BrushType type);
+
+    QColor getColor();
+    int getBrushSize();
+    BrushType getBrushType();
+
+private:
+    int brushSize;
+    QColor brushColor;
 };
 
 #endif // BRUSH_H
