bfa223f2cc469f2c9cb8fe93873c786a29f0dcaa
Brayden Newsom <braydennewsom@braydens-mbp-2.uconnect.utah.edu>
2023-03-29 10:15:26 -0600

    Fixed latest push so program can now run

--- animationframes.cpp
+++ animationframes.cpp
@@ -1,6 +1,32 @@
 #include "animationframes.h"
-
+/*
 AnimationFrames::AnimationFrames()
 {
 
 }
+*/
+
+QImage* AnimationFrames::addFrame()
+{
+
+}
+
+QImage* AnimationFrames::getFrame(int index)
+{
+
+}
+
+void AnimationFrames::deleteFrame(int index)
+{
+
+}
+
+int AnimationFrames::getFrameCount()
+{
+
+}
+
+
+
+
+
