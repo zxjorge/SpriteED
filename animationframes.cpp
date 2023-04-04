fef5c6ae2c9de2d8a8d46b3b43b8f03c2577c7ac
Arkein Singh <60797452+singark21@users.noreply.github.com>
2023-04-05 01:07:52 +0300

    Cannot clear if frames are running

--- animationframes.cpp
+++ animationframes.cpp
@@ -181,8 +181,9 @@ int AnimationFrames::getSelectedIndex(){
 
 /// @brief 
 void AnimationFrames::clearSelectedFrame() {
-    frames.at(selectedIndex).fill(Qt::white);
-    emit frameCleared();
+    if(!animTimer.isActive())
+        frames.at(selectedIndex).fill(Qt::white);
+        emit frameCleared();
 }
 
 /// @brief
